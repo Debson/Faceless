575ec9d19b2e8706d4335693476ee39b12695f82
Debson <michd3ael@gmail.com>
2017-10-15 01:27:22 +0100

    Add empty project

---
+++ Warrior/Assets/Code/Movement/MovementController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementController : MonoBehaviour
+{
+    public float desiredWalkDirection;
+    public static int playerPoints = 0;
+
+    [SerializeField]
+    float walkSpeed = 50;
+
+    Rigidbody2D myBody;
+
+    protected void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    protected void FixedUpdate()
+    {
+        float desiredXVelocity = desiredWalkDirection * walkSpeed * Time.deltaTime;
+
+        myBody.velocity = new Vector2(desiredXVelocity, myBody.velocity.y);
+    }
+
+}
