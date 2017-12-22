bda0a784e6c975641b9539039a052001f66b4fce
Debson <michd3ael@gmail.com>
2017-12-22 03:05:17 +0000

    progress in level4

---
+++ Warrior/Assets/Code/Camera/BackgroundFollowsPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFollowsPlayer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject background;
+
+    PlayerController player;
+
+    protected void Awake()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    protected void LateUpdate ()
+    {
+        background.transform.position = player.transform.position;
+	}
+}
